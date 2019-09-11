7c1f151c7b9d73ddb6e8f2c557e5aba735b6b7f6
João Carias <nb22771@novabase.pt>
2019-09-11 16:10:08 +0100

    Added StarRaing component
    Signed-off-by: João Carias <nb22771@novabase.pt>

--- example/src/App.js
+++ example/src/App.js
@@ -3,11 +3,45 @@ import React, { Component } from "react";
 import ExampleComponent, { StarRating } from "jc-react-components-library";
 
 export default class App extends Component {
+  state = {
+    rating: 0,
+    isRateGiven: false
+  };
+
+  handleSelection = value => {
+    this.setState({ rating: value, isRateGiven: true });
+  };
+
+  resetRating = () => {
+    this.setState({ rating: 0, isRateGiven: false });
+  };
+
   render() {
     return (
       <div>
-        <StarRating />
-        <ExampleComponent text="Modern React component module" />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            color: this.state.isRateGiven ? "gold" : "slateGray"
+          }}
+        >
+          <StarRating
+            totalStars={5}
+            starSize={96}
+            handleSelection={this.handleSelection}
+            readOnly={false}
+            value={this.state.rating}
+          />
+        </div>
+        {this.state.isRateGiven && (
+          <React.Fragment>
+            <ExampleComponent
+              text={`Current selected value ${this.state.rating}`}
+            />
+            <button onClick={this.resetRating}>Reset</button>
+          </React.Fragment>
+        )}
       </div>
     );
   }
